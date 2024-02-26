c5f77e5c21b12aa46b903a089a4b188a5726bb23
Jose Luis Izquierdo Hernandez <110314046+jocl0110@users.noreply.github.com>
2024-02-26 17:25:29 -0600

    Create an unordered list using JSX

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -3,33 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const todoList = [
+  {
+    id: 1,
+    title: "Start the lesson"
+  },
+  {
+    id: 2,
+    title: "Finish the lesson"
+  },
+  {
+    id: 3,
+    title: "Complete assignment"
+  }
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+  
+  return (  
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+        return <li key="item.id">{item.title}</li>;
+      })}
+      </ul>
+    </div>
+  );
 }
 
 export default App
