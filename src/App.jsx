1e19aeee2e6544200a46b331dd82de79a5d6c7d1
Jose Luis Izquierdo Hernandez <110314046+jocl0110@users.noreply.github.com>
2024-04-09 15:56:31 -0500

    Use camelCase

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TodoList from './TodoList'
 import './AddTodoForm'
 import AddTodoForm from './AddTodoForm'
 
-const usesemiPersistentState = () => {
+const useSemiPersistentState = () => {
 
   const [todoList, setTodoList] = React.useState(() => {
 
@@ -23,7 +23,7 @@ const usesemiPersistentState = () => {
 
 function App() {
 
-  const [todoList, setTodoList] = usesemiPersistentState();
+  const [todoList, setTodoList] = useSemiPersistentState();
   
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
