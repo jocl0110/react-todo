8746c132535ebb8d9bd855f55c31248609b09f13
Jose Luis Izquierdo Hernandez <110314046+jocl0110@users.noreply.github.com>
2024-06-07 23:16:19 -0500

    Rename and type definitions

---
+++ src/TodoListPage.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import AddTodoForm from './components/AddTodoForm';
+import TodoList from './components/TodoList';
+
+
+interface Todo {
+  id: string;
+  title: string;
+}
+interface TodoListPageProps {
+  addTodo: (newTodo: Todo) => void;
+  removeTodo: (id: string) => void;
+  todoList: Todo;
+  isLoading: boolean;
+}
+
+const TodoListPage: React.FC<TodoListPageProps> = ({addTodo, removeTodo, todoList, isLoading}) => {
+ 
+  return (
+    <>
+      <h1 className='header'>Todo List</h1>
+      <AddTodoForm onAddTodo={addTodo} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
+    </>
+  );
+}
+
+
+
+export default TodoListPage;
